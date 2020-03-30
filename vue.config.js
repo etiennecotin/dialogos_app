8a5610d23f8375ff3e8c73b9b77d9217b5c40fc6
Etienne Cotin <etienne.cotin@gmail.com>
2020-03-30 16:10:24 +0200

    add minify js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,28 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
 module.exports = {
-  configureWebpack:{
+  configureWebpack: {
     optimization: {
       splitChunks: {
         minSize: 10000,
-        maxSize: 200000,
-      }
+        maxSize: 200000
+      },
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            warnings: false,
+            parse: {},
+            compress: {},
+            mangle: true, // Note `mangle.properties` is `false` by default.
+            output: null,
+            toplevel: false,
+            nameCache: null,
+            ie8: false,
+            keep_fnames: false
+          },
+          cache: true
+        })
+      ]
     }
   },
   css: {
